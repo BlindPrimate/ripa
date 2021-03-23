87201d228edcfb41a4596ac0854c7e1cb7932c7e
ecleek <eric_cleek@steris.com>
2021-03-23 10:56:51 -0400

    removal of keys

--- ripabug/settings.py
+++ ripabug/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zb%$wss6(o=*2=6nxag&o)cwpq!vn^oynk6y0z6zj!0+&7^w=u'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
